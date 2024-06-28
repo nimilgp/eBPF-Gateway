0ed4b59ac3cfc4af306bdeed4a116c174d7234dd
nimil <nimilgeorgepradeepngp@gmail.com>
2024-06-28 15:35:54 +0530

    added bearer token verify and update

--- srv/bearertokens.go
+++ srv/bearertokens.go
@@ -44,3 +44,35 @@ func (app *application) generateBearerToken(w http.ResponseWriter, acc dbLayer.A
 		return
 	}
 }
+
+func (app *application) verifyAndUpdateBearerToken(tokenString string) bool {
+	bearerToken, err := app.queries.RetrieveBearerToken(app.ctx, tokenString)
+	if err != nil {
+		log.Printf("<ERROR>\t\t[(verify & update bearer token)failed to retrieve bearer token]\n%s\n\n", err)
+		return false
+	}
+	curTime := time.Now()
+	if curTime.After(bearerToken.Validtill.Time) {
+		validTill := curTime.Add(time.Minute * 30)
+		timeStamp := pgtype.Timestamp{
+			Time:  validTill,
+			Valid: true,
+		}
+		arg := dbLayer.UpdateBearerTokenExpirationParams{
+			Tokenstring: tokenString,
+			Validtill:   timeStamp,
+		}
+		if err := app.queries.UpdateBearerTokenExpiration(app.ctx, arg); err != nil {
+			log.Printf("<ERROR>\t\t[(verify & update bearer token)failed to update bearer token]\n%s\n\n", err)
+			return false
+		}
+		log.Printf("<INFO>\t\t[(verify & update bearer token)updated bearer token expiration]\ntoken sting :%s\n\n", tokenString)
+		return true
+	} else {
+		log.Printf("<INFO>\t\t[(verify & update bearer token)bearer token has expired]\ntoken sting :%s\n\n", tokenString)
+		if err := app.queries.DeleteBearerToken(app.ctx, bearerToken.Username); err != nil {
+			log.Printf("<ERROR>\t\t[(verify & update bearer token)failed to delete beare token]\n%s\n\n", err)
+		}
+		return false
+	}
+}
